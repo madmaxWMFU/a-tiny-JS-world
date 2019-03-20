1096e9acfc5a88a763089e368d75ffde1ce54b7f
madmaxWMFU <yasmaks@gmail.com>
2019-03-20 16:51:12 +0200

    create obj

--- index.js
+++ index.js
@@ -7,8 +7,50 @@
 
 // ======== OBJECTS DEFINITIONS ========
 // Define your objects here
+const man = {
+    name: "Max",
+    gender: "male",
+    species: "human",
+    hands: "2",
+    legs: "2",
+    say: () => `Hallo!`,
+}
+
+const woman = {
+    name: "Lena",
+    gender: "female",
+    species: "human",
+    hands: "2",
+    legs: "2",
+    say: () => `Bonjour!`,
+}
+
+const dog = {
+    name: "Adolf",
+    gender: "male",
+    species: "dog",
+    hands: "0",
+    legs: "4",
+    say: () => `Woof!`,
+}
+
+const cat = {
+    name: "Klara",
+    gender: "male",
+    species: "cat",
+    hands: "0",
+    legs: "4",
+    say: () => `Meeow!`,
+}
+
+print(`Hey there, my name is <strong>${dog.name}</strong> and I am a ${dog.species}! My gender is ${dog.gender}, in addition I have ${dog.legs} legs. Wait a minute...${dog.say()}!`);
 
 
+
+// Hey there, my name is Paolo and I am a dog!My gender is male, in addition I have 4 legs.Wait a minute...Bark!
+// Hey there, my name is Sophie and I am a cat!My gender is female, in addition I have 4 legs.Wait a minute...Mur - meow ?
+// Hey there, my name is Pablito and I am a human!My gender is male, in addition I have 2 legs and 2 hands.Wait a minute...It seems that we got a strange rat here...Hey there, my name is Twitch and I am a rat!My gender is male, in addition I have 2 legs and 2 hands.Wait a minute...Oh, HELLO!
+// Hey there, my name is Binokia and I am a human!My gender is female, in addition I have 2 legs and 2 hands.Wait a minute...Mur - meow ?
 // ======== OUTPUT ========
 /* Use print(message) for output.
    Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
@@ -27,5 +69,3 @@
    print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
    print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
    */
-
-
